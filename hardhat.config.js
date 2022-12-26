044701b7e79ca9014c2642ef0809a156561e7617
sanam-umer <sanamummer1@gmail.com>
2022-12-26 11:45:27 +0530

    edited

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,18 +4,14 @@ require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: 'ganache',
   networks: {
     ganache: {
-      url: 'https://localhost:8545'
-    },
-    goerli:{
-      url:'https://goerli.infura.io/v3/999f7b04821a4e28acbe1c5ddcf43baf',
-      accounts:['fe3fb6a3a2322072a1319177dc2025a53fe7dde05734260f34bf3e757f7764c3']
+      url: 'http://localhost:8545'
     }
   },
   solidity: "0.8.17",
 };
+
 
--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -3,7 +3,7 @@ const {ethers,upgrades}= require('hardhat');
 async function main() {
   
   const SampleV2 = await ethers.getContractFactory("AgreementContractV2");
-  const samplev2 = await upgrades.upgradeProxy('0x4436b33d8364bddeA73490F6a8a31da70Ec32a72',SampleV2);
+  const samplev2 = await upgrades.upgradeProxy('0x327B5Cc65f23494eCD1774B3Fb7C9d1Ab90cb1fd',SampleV2);
 
   await samplev2.deployed();
 
